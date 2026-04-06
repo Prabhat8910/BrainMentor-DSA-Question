88278503582e952bca9f1e7852f706ceefaaee33
Prabhat Kaushal <prabhatkaushal8910@gmail.com>
2026-04-06 23:45:35 +0530

    update

--- FindFrequency.java
+++ FindFrequency.java
@@ -1,6 +1,25 @@
+
+import java.util.HashMap;
+
 public class FindFrequency {
     public static void main(String[] args) {
-    
+        int num = 724272;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+
+        while(num!=0){
+            int digit = num%10;
+            if (map.get(digit) == null){
+                map.put(digit, 1);
+            }
+            else{
+                int count = map.get(digit);
+                count++;
+                map.put(digit, count);
+            }
+            num = num/10;
+        }
+        System.out.println(map);
     }
     
 }
